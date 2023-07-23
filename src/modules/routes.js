726e0dd213bd9154e0caf0c72ae3884a20b35a11
FotihYusupov <yusupovfotih158@gmai.com>
2023-07-23 18:02:28 +0500

    routes bug fixed routes.js

--- src/modules/routes.js
+++ src/modules/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import usersRoute from "./users/routes.js";
 import postsRoutes from "./posts/routes.js"
+import categoriesRoute from "./categories/routes.js"
 
 const routes = Router();
 
 export default routes
     .use(usersRoute)
     .use(postsRoutes)
+    .use(categoriesRoute)
